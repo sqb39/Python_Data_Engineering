f05be0a04610ad6e7b87c1df7606d275fa0ea514
Md Nazmus  Saqib <saqib@Saqibs-MacBook-Air.local>
2024-07-02 15:57:59 +0300

    Finalisation of the  project

--- ETL_Project/etl_project.py
+++ ETL_Project/etl_project.py
@@ -1,7 +1,5 @@
 # Code for ETL operations on Country-GDP data
-
 # Importing the required libraries
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
@@ -9,78 +7,86 @@ import numpy as np
 import sqlite3
 from datetime import datetime 
 
-def extract(url, table_attribs):
-    ''' The purpose of this function is to extract the required
-    information from the website and save it to a dataframe. The
-    function returns the dataframe for further processing. '''
 
+def log_progress(message):
+    ''' This function logs the mentioned message of a given stage of the
+    code execution to a log file. Function returns nothing'''
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./code_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n') 
+
+def extract(url, table_attribs):
+    ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
     page = requests.get(url).text
-    data = BeautifulSoup(page,'html.parser')
+    data = BeautifulSoup(page, 'html.parser')
     df = pd.DataFrame(columns=table_attribs)
     tables = data.find_all('tbody')
-    rows = tables[2].find_all('tr')
+    rows = tables[0].find_all('tr')
+
     for row in rows:
-        col = row.find_all('td')
-        if len(col)!=0:
-            if col[0].find('a') is not None and '—' not in col[2]:
-                data_dict = {"Country": col[0].a.contents[0],
-                             "GDP_USD_millions": col[2].contents[0]}
-                df1 = pd.DataFrame(data_dict, index=[0])
-                df = pd.concat([df,df1], ignore_index=True)
+        cols = row.find_all('td')
+        if len(cols)!=0:
+            data_dict = {"Name": cols[1].find_all('a')[1]['title'], #ANSWER FOR 1
+            # "MC_USD_Billion": float(cols[2].contents[0].strip())}
+            "MC_USD_Billion": float(cols[2].contents[0][:-1])} #ANSWER FOR 2
+            print(data_dict)
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df,df1], ignore_index=True)
+    #print(df)
+
     return df
 
-def transform(df):
-    ''' This function converts the GDP information from Currency
-    format to float value, transforms the information of GDP from
-    USD (Millions) to USD (Billions) rounding to 2 decimal places.
-    The function returns the transformed dataframe.'''
 
-    GDP_list = df["GDP_USD_millions"].tolist()
-    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
-    GDP_list = [np.round(x/1000,2) for x in GDP_list]
-    df["GDP_USD_millions"] = GDP_list
-    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+    file_path = './exchange_rate.csv'
+    df2 = pd.read_csv(file_path)
+    df2 = df2.set_index('Currency')
+    exchange_rate = df2['Rate'].to_dict()
+
+    df['MC_GBP_Billion'] = [np.round(x*exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
+    df['MC_EUR_Billion'] = [np.round(x*exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(x*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
+
     return df
 
-def load_to_csv(df, csv_path):
-    ''' This function saves the final dataframe as a `CSV` file 
-    in the provided path. Function returns nothing.'''
 
-    df.to_csv(csv_path)
+def load_to_csv(df, output_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
+    df.to_csv(output_path)
+
 
 def load_to_db(df, sql_connection, table_name):
-    ''' This function saves the final dataframe to as a database table
-    with the provided name. Function returns nothing.'''
-
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
     df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
-def run_query(query_statement, sql_connection):
-    ''' This function runs the stated query on the database table and
-    prints the output on the terminal. Function returns nothing. '''
 
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
     print(query_statement)
     query_output = pd.read_sql(query_statement, sql_connection)
     print(query_output)
 
-def log_progress(message):
-    ''' This function logs the mentioned message at a given stage of the 
-    code execution to a log file. Function returns nothing.'''
 
-    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
-    now = datetime.now() # get current timestamp 
-    timestamp = now.strftime(timestamp_format) 
-    with open("./etl_project_log.txt","a") as f: 
-        f.write(timestamp + ' : ' + message + '\n')    
-
-''' Here, you define the required entities and call the relevant 
+''' Here, you define the required entities and call the relevant
 functions in the correct order to complete the project. Note that this
 portion is not inside any function.'''
 
-url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
-table_attribs = ["Country", "GDP_USD_millions"]
-db_name = 'World_Economies.db'
-table_name = 'Countries_by_GDP'
-csv_path = './Countries_by_GDP.csv'
+url = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ["Name", "MC_USD_Billion"]
+db_name = 'Banks.db'
+table_name = 'Largest_banks'
+csv_path = './Largest_banks_data.csv'
 
 log_progress('Preliminaries complete. Initiating ETL process')
 
@@ -88,7 +94,7 @@ df = extract(url, table_attribs)
 
 log_progress('Data extraction complete. Initiating Transformation process')
 
-df = transform(df)
+df = transform(df, csv_path)
 
 log_progress('Data transformation complete. Initiating loading process')
 
@@ -96,17 +102,26 @@ load_to_csv(df, csv_path)
 
 log_progress('Data saved to CSV file')
 
-sql_connection = sqlite3.connect('World_Economies.db')
+sql_connection = sqlite3.connect(db_name)
 
 log_progress('SQL Connection initiated.')
 
 load_to_db(df, sql_connection, table_name)
 
-log_progress('Data loaded to Database as table. Running the query')
+log_progress('Data loaded to Database as table. Executing queries')
 
-query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+query_statement = f"SELECT * from {table_name}"
 run_query(query_statement, sql_connection)
+log_progress('Process Complete.')
 
+query_statement = f"SELECT AVG(MC_GBP_Billion) FROM {table_name}"
+run_query(query_statement, sql_connection)
+log_progress('Process Complete.')
+
+query_statement = f"SELECT Name from {table_name} LIMIT 5 "
+run_query(query_statement, sql_connection)
 log_progress('Process Complete.')
 
 sql_connection.close()
+
+log_progress('Server connection closed.')
